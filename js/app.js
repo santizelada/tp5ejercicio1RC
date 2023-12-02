1dad4c516d95506bf0d41e7f6c1e9037508bb2a2
santiago <santtyzelada@gmail.com>
2023-12-02 19:27:03 -0300

    First commit

---
+++ js/app.js
@@ -0,0 +1,9 @@
+// const cambiarTitulo = () => {
+//     const titulo = document.querySelector("h1")
+//     // const titulo = document.querySelector("#algunID")
+
+//     const muchosParrafos = document.querySelectorAll("p")
+
+    
+//     console.log(muchosParrafos)
+// }
