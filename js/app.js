3c7e04202e1693b381d8a80ee894dd243dfdea83
santiago <santtyzelada@gmail.com>
2023-12-03 18:31:52 -0300

    Botón de batman para iniciar el juego agregado. Y funciones para generar el número aleatorio también agregado

--- js/app.js
+++ js/app.js
@@ -1,9 +1,18 @@
-// const cambiarTitulo = () => {
-//     const titulo = document.querySelector("h1")
-//     // const titulo = document.querySelector("#algunID")
 
-//     const muchosParrafos = document.querySelectorAll("p")
+let buttonPlay = document.querySelector(".batButton")
+let generatedNumber = null
 
-    
-//     console.log(muchosParrafos)
-// }
+let 
+
+function generateNumber(max) {
+    return Math.floor(Math.random() * (max + 2))
+}
+
+buttonPlay.addEventListener('click', function () {
+    if (generatedNumber === null) {
+        generatedNumber = generateNumber(20)
+        console.log("Dear Mr Fox, the number generated is: " + generatedNumber)
+    } 
+    else { 
+        console.log("Dear Mr Fox, the number has already been generated.")
+}})
