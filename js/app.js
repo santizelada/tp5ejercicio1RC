15680ee1ed326c67f6c97914e0e8359b955ed2cf
santiago <santtyzelada@gmail.com>
2023-12-04 11:59:49 -0300

    Lógica terminada del juego

--- js/app.js
+++ js/app.js
@@ -1,8 +1,37 @@
-
+let sendButton = document.querySelector("#sendButton")
 let buttonPlay = document.querySelector(".batButton")
 let generatedNumber = null
+let userAttempt;
 
-let 
+
+
+sendButton.addEventListener('click', function () {
+    
+    userAttempt = document.getElementById("guessButton").value
+
+
+    if (generatedNumber === null || generatedNumber === undefined) {
+        alert("Dear user, please press the bat button to generate a secret number")
+    }
+    else if (userAttempt === null || userAttempt.trim() === '') {
+        alert("Please, type in a number")
+    } else if (userAttempt > generatedNumber) {
+        alert('The number is higher. Try again.')
+    } else if (userAttempt < generatedNumber) {
+        alert('The number is lower. Try again')
+    } else if (userAttempt == generatedNumber) {
+        alert("Congrats! You've guessed the number")
+    }
+    else {
+        generatedNumber = generateNumber(20)
+        console.log("Dear user, the number has already been generated.")
+    }
+
+})
+
+
+
+
 
 function generateNumber(max) {
     return Math.floor(Math.random() * (max + 2))
@@ -11,8 +40,15 @@ function generateNumber(max) {
 buttonPlay.addEventListener('click', function () {
     if (generatedNumber === null) {
         generatedNumber = generateNumber(20)
-        console.log("Dear Mr Fox, the number generated is: " + generatedNumber)
-    } 
-    else { 
-        console.log("Dear Mr Fox, the number has already been generated.")
-}})
+        console.log("Dear user, the number generated is: " + generatedNumber)
+    }
+    else {
+        console.log("Dear user, the number has already been generated.")
+    }
+})
+
+function numeroAdivinado () {
+    console.log(userAttempt)
+}
+
+numeroAdivinado()
